1b8a3abd2cb93c4dcbbaea3c46b994551a285f18
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-16 22:47:52 +0100

    b

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,11 +1,3 @@
--- Check if the required arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
+-- script that lists all the tables of a database in your MySQL server
 
--- Store the provided arguments in variables
-database="$1"
-
--- Execute the MySQL command with the provided username and database name
-mysql -u "$username" -p -D "$database" -e "SHOW TABLES;"
+SHOW TABLES FROM $1;
