4b21066bfa9db0b93218d6eae97070e572830e1a
HMDT2023 <merveilhouenagnon@gmail.com>
2023-08-16 23:14:14 +0100

    sq

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 --Write a script that creates a table called first_table in the current database in your MySQL server.
+CREATE DATABASE hbtn_test_db_4;
 
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
